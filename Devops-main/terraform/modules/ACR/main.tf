a62e73c5748374f22904d88da259ce7b76a5a85e
rkannagala <93792117+rkannagala@users.noreply.github.com>
2022-02-07 15:58:13 -0600

    Create main.tf
    ACR Module

---
+++ Devops-main/terraform/modules/ACR/main.tf
@@ -0,0 +1,7 @@
+resource "azurerm_container_registry" "acr" {
+  name                = var.acr_name
+  location            = var.location
+  resource_group_name = var.rgname
+  sku                 = "Premium"
+  admin_enabled       = true
+}
